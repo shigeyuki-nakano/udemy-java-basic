e284f4bf62427c3efc8ca2055b8bc633b538e9cb
shigeyuki-nakano <thuyajino_in_the_groove@outlook.jp>
2022-10-10 13:22:07 +0900

    総合演習:自販機プログラムの作成

---
+++ src/ComEx1/Jihan/entity/model/Coins.java
@@ -0,0 +1,34 @@
+package ComEx1.Jihan.entity.model;
+
+public enum Coins {
+    ItiYen(1),
+    GoYen(5),
+    JuYen(10),
+    GoJuYen(50),
+    HyakuYen(100),
+    GoHyakuYen(500);
+
+    private final int coin;
+
+    Coins(int coin) {
+        this.coin = coin;
+    }
+
+    public static Coins getByInt(int id) {
+        for(Coins coin : Coins.values()) {
+            if(id == coin.coin) {
+                return coin;
+            }
+        }
+
+        return null;
+    }
+
+    public String toString() {
+        return coin + "円玉";
+    }
+
+    public int getCoin() {
+        return coin;
+    }
+}
---
+++ src/ComEx1/Jihan/presentation/validation/CoinValidator.java
@@ -0,0 +1,19 @@
+package ComEx1.Jihan.presentation.validation;
+
+import ComEx1.Jihan.entity.model.Coins;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CoinValidator {
+    public static boolean isAvailable(Coins coin) {
+        List<Coins> availableCoins = Arrays.asList(
+                Coins.GoJuYen,
+                Coins.GoJuYen,
+                Coins.HyakuYen,
+                Coins.GoHyakuYen
+        );
+
+        return availableCoins.contains(coin);
+    }
+}
